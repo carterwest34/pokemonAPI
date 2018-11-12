ec1c57827aeb8124963a051abd69fadde26c4d2e
Carter West <carterwest@MacBook-Pro-67.local>
2018-11-12 12:25:02 -0600

    Added UI Elements and connected all outlets and started on actions

---
+++ Pods/Target Support Files/Pods-pokemonAPI/Pods-pokemonAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokemonAPI : NSObject
+@end
+@implementation PodsDummy_Pods_pokemonAPI
+@end
--- pokemonAPI/ViewController.swift
+++ pokemonAPI/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    //MARK: IBOutlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var textView: UITextView!
+    
+    //setting up base url to be added to for call
+    let pokemonAPIBaseUrl = "https://pokeapi.co/v2/"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
-
+    
+    @IBAction func searchButtonTapped(_ sender: Any) {
+        textField.resignFirstResponder()
+        
+        guard
+            let pokemonTextField = textField.text, !pokemonTextField.isEmpty else {return}
+        
+        //clear out text field upon button press
+        textField.text = ""
+        
+        let pokemonNameComponent = pokemonTextField.replacingOccurrences(of: "", with: "+")
+        
+        let requestURL = pokemonAPIBaseUrl + pokemonNameComponent
+        
+    }
+    
 
 }
 
