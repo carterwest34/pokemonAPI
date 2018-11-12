b5066d92991b91975e292a3357d1eb8e250aa8ef
Carter West <carterwest@MacBook-Pro-67.local>
2018-11-12 13:17:37 -0600

    JSON and requests are working properly

--- pokemonAPI/ViewController.swift
+++ pokemonAPI/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     
     //setting up base url to be added to for call
-    let pokemonAPIBaseUrl = "https://pokeapi.co/v2/"
+    let pokemonAPIBaseUrl = "https://pokeapi.co/api/v2/pokemon/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +35,20 @@ class ViewController: UIViewController {
         //clear out text field upon button press
         textField.text = ""
         
-        let pokemonNameComponent = pokemonTextField.replacingOccurrences(of: "", with: "+")
+        let pokemonNameComponent = pokemonTextField.replacingOccurrences(of: " ", with: "+")
         
         let requestURL = pokemonAPIBaseUrl + pokemonNameComponent
         
+        Alamofire.request(requestURL).responseJSON { (resonse) in
+            switch resonse.result {
+            case .success(let value):
+                let json = JSON(value)
+                self.textView.text = json["name"].stringValue 
+            case .failure(let error):
+                self.textView.text = "Invalid selection entered or error occured. Please try again."
+                print(error.localizedDescription)
+            }
+        }
     }
-    
-
 }
 
